ed892993aac139870919d4e99b03fb050e0e935f
Aashish Anantharaman <68557624+ABlizzard07@users.noreply.github.com>
2024-03-27 16:57:40 -0400

    responsiveness

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,10 @@
 export default function Contact() {
     return (
-        <div>
+        <div className="bg-emerald-100 flex flex-col h-screen mb-8">
             <h1 className="text-4xl font-bold text-center my-8">Contact Us</h1>
             <iframe 
                 src="https://forms.gle/hKw3Qnc3L33m1nTm9" 
-                className="h-100 w-100 sm:h-120 sm:w-120 md:h-140 md:w-140 lg:h-160 lg:w-160 mx-2" >
+                className="flex-grow w-100 sm:w-120 md:w-140 lg:w-160 mx-2" >
                 Loading…
             </iframe>
         </div>
--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { createClient } from 'contentful';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
@@ -26,24 +25,22 @@ export default function Docs() {
 
   if (selectedPost) {
     return (
-      <div className="w-full flex flex-col items-center justify-center">
-        <button onClick={() => setSelectedPost(null)}>Back</button>
-        <h2 className="text-2xl font-bold">{selectedPost.fields.title}</h2>
-        <p>{selectedPost.fields.description}</p>
-        {selectedPost.fields.body && documentToReactComponents(selectedPost.fields.body)}
+      <div className="w-full flex flex-col items-center justify-center mt-6 bg-emerald-100">
+        <h2 className="text-3xl font-bold">{selectedPost.fields.title}</h2>
+        <button className="mt-4 rounded-2xl border border-emerald-700 px-5" onClick={() => setSelectedPost(null)}>Back</button>
+        <p className="w-2/3 text-lg mt-4">{selectedPost.fields.body}</p>
       </div>
     );
   }
 
   return (
-    <div className="w-full flex flex-col items-center justify-center mt-6">
+    <div className="w-full flex flex-col items-center justify-center mt-6 bg-emerald-100">
       <h1 className="text-3xl font-bold text-center">Green Energy</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 mx-8 mt-8">
         {posts.map((post, index) => (
-          <div key={index} className="border p-4" onClick={() => setSelectedPost(post)}>
+          <div key={index} className="border border-emerald-700 rounded-xl p-4 hover:bg-emerald-200" onClick={() => setSelectedPost(post)}>
             <h2 className="text-xl font-bold">{post.fields.title}</h2>
-            <p>{post.fields.description}</p>
-            {post.fields.body && documentToReactComponents(post.fields.body)}
+            <p>{post.fields.body && post.fields.body.substring(0, 500)}</p>
           </div>
         ))}
       </div>
