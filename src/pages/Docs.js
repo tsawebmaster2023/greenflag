ee64b04974349fa2c0724e876e2c008a7717545c
Aashish Anantharaman <68557624+ABlizzard07@users.noreply.github.com>
2024-04-02 00:56:17 -0400

    fixed docs markdown and sources

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { createClient } from 'contentful';
+import ReactMarkdown from 'react-markdown';
 
 const client = createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
@@ -28,19 +29,38 @@ export default function Docs() {
       <div className="w-full flex flex-col items-center justify-center mt-6 bg-emerald-100">
         <h2 className="text-3xl font-bold">{selectedPost.fields.title}</h2>
         <button className="mt-4 rounded-2xl border border-emerald-700 px-5" onClick={() => setSelectedPost(null)}>Back</button>
-        <p className="w-2/3 text-lg mt-4">{selectedPost.fields.body}</p>
+        <ReactMarkdown className="w-2/3 text-lg mt-4" components={{
+          h1: ({node, ...props}) => <h1 {...props} className="font-bold"/>,
+          h2: ({node, ...props}) => <h2 {...props} className="font-bold"/>,
+          h3: ({node, ...props}) => <h3 {...props} className="font-bold"/>,
+          p: ({node, ...props}) => <p {...props} className="mb-4"/>,
+        }}>{selectedPost.fields.body}</ReactMarkdown>
+        <ReactMarkdown components={{
+          a: ({node, ...props}) => <a {...props} style={{display: 'block', marginBottom: '10px'}}/>
+        }}>{selectedPost.fields.sources}</ReactMarkdown>
       </div>
     );
   }
 
+  function shortenText(text) {
+    let words = text.split(' ');
+    words[words.length - 1] = '...';
+    return words.join(' ');
+  }
+
   return (
-    <div className="w-full flex flex-col items-center justify-center mt-6 bg-emerald-100">
+    <div className="w-full flex flex-col items-center justify-center my-6 bg-emerald-100">
       <h1 className="text-3xl font-bold text-center">Green Energy</h1>
       <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 mx-8 mt-8">
         {posts.map((post, index) => (
           <div key={index} className="border border-emerald-700 rounded-xl p-4 hover:bg-emerald-200" onClick={() => setSelectedPost(post)}>
             <h2 className="text-xl font-bold">{post.fields.title}</h2>
-            <p>{post.fields.body && post.fields.body.substring(0, 500)}</p>
+            <ReactMarkdown components={{
+              h1: ({node, ...props}) => <h1 {...props} className="font-bold"/>,
+              h2: ({node, ...props}) => <h2 {...props} className="font-bold"/>,
+              h3: ({node, ...props}) => <h3 {...props} className="font-bold"/>,
+              p: ({node, ...props}) => <p {...props} className="mb-5"/>,
+            }}>{post.fields.body && shortenText(post.fields.body.substring(0, 350))}</ReactMarkdown>
           </div>
         ))}
       </div>
